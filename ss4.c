db53df6d63d0b5afe2e876a21bff28db93698fe3
SENTHILKUMARAN2 <36395326+SENTHILKUMARAN2@users.noreply.github.com>
2019-02-18 16:30:59 +0530

    Create ss4.c

---
+++ ss4.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+int main()
+	{
+  char s;
+	scanf("%c",&s);
+	if(s >='a' && s <= 'z' || s >= 'A' && s <= 'Z')
+	printf("alphabet");
+	else
+	printf("not alphabet");
+     return 0;
+}
